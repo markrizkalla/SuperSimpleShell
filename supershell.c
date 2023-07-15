f8a4e0b02d2fa7c766f883ad3a9f8c498565660e
markrizkalla <mark.rizkalla1@gmail.com>
2023-07-15 08:25:41 +0300

    simple shell

--- supershell.c
+++ supershell.c
@@ -1,13 +1,43 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 
 int main(void)
 {
 
-	pid_t my_pid;
+	pid_t pid;
+	pid = fork();
 
-	my_pid = getppid();
-	printf("%u\n", my_pid);
+	while(pid != 0){
+	
+		wait(NULL);
+		pid = fork();
+	}
+
+	char * buffer = malloc(1024);
+	size_t len = 1024;
+
+
+	if(pid == 0){
+	      
+		 while(1){
+
+                printf("$ ");
+
+                getline(&buffer , &len , stdin);
+
+                 buffer[strcspn(buffer, "\n")] = 0;
+
+                char *argv[] = {buffer,NULL,"/usr/",NULL};
+
+                      execve(argv[0], argv, NULL);
+               }	
+	}
 	return (0);
 
 }
